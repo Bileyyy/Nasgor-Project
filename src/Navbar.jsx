cf03755564e2658dff31a927eba4e9f2d7abcd35
Bileyyy <183796742+Bileyyy@users.noreply.github.com>
2025-02-07 14:22:51 +0700

    lr

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
-      <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 flex justify-center items-center bg-amber-100 px-6 py-2 rounded-full shadow-lg w-full max-w-[80%] md:max-w-[60%]">
-        <div className="menu flex flex-wrap justify-center items-center gap-6 md:gap-10">
+      <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 flex justify-center items-center bg-amber-100 px-4 py-2 rounded-full shadow-lg w-full max-w-[70%] md:max-w-[50%] lg:max-w-[35%]">
+        <div className="menu flex flex-wrap justify-center items-center gap-4 md:gap-6">
           <ul className="flex">
             <li>
               <a href="#about" className="hover:text-white font-inter text-sm md:text-md font-bold">About Us</a>
