bbe5d457317973988aade02ac534074d8d42978f
najiyawp <najiyarizqi70@gmail.com>
2025-02-07 10:47:54 +0700

    fixed

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth, db } from "../firebase";
+import { auth, db } from "./firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
